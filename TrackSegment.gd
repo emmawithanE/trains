ca004bb1144f52f0a3c4314af43a6f5490f61f54
emmawithanE <emma@catgirl.software>
2024-10-16 11:00:26 +1030

    works in 3d now :)

--- TrackSegment.gd
+++ TrackSegment.gd
@@ -1,8 +1,13 @@
 extends Node3D
-class_name TrackSegment
+class_name TrackSegment3D
 
 
 @export var paths : Array[Path3D]
 
-func get_track_path(num := 0):
-	return paths[num]
+func get_track_path(num := 0) -> Path3D:
+	if num <= len(paths):
+		return paths[num]
+	else:
+		return paths[0]
+
+@export var next_segment : TrackSegment3D
--- train.gd
+++ train.gd
@@ -1,7 +1,45 @@
-extends PathFollow3D
-@export var speed := 1.0
-@export var starting_track : TrackSegment
+extends Node3D
+class_name Train3D
 
+@export var starting_segment : TrackSegment3D
+
+var current_segment : TrackSegment3D
+
+var running := false
+const SPEED = 2
+
+func _ready() -> void:
+	if starting_segment:
+		$Path3D.curve = get_global_path(starting_segment)
+		current_segment = starting_segment
+		running = true
+
+func get_global_path(segment : TrackSegment3D) -> Curve3D:
+	var curve := segment.get_track_path().curve.duplicate()
+	for i in range(curve.point_count):
+		curve.set_point_position(i, segment.to_global(curve.get_point_position(i)))
+	return curve
+	
 
 func _process(delta: float) -> void:
-	progress += speed * delta
+	if running:
+		$Path3D/PathFollow3D.progress += SPEED * delta
+	if $Path3D/PathFollow3D.progress_ratio > 0.95:
+		append_next_segment()
+
+func append_next_segment():
+	if !current_segment.next_segment:
+		return
+	var next_segment := current_segment.next_segment
+	var next_path := get_global_path(next_segment)
+	
+	$Path3D.curve.set_point_out($Path3D.curve.point_count - 1, next_path.get_point_out(0))
+	
+	for i in range(1,next_path.point_count):
+		$Path3D.curve.add_point(
+			next_path.get_point_position(i),
+			next_path.get_point_in(i),
+			next_path.get_point_out(i)
+		)
+	
+	current_segment = next_segment
